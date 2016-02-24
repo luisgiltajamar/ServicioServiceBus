62e5d665a11e06d0ad9af98ff6ba11da928d9924
Luis Felipe Gil Lamaignere <luis.gil@toolents.com>
2016-02-24 11:19:13 +0000

    fin

---
+++ ServicioServiceBus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+
+namespace ServicioServiceBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host=new ServiceHost(typeof(ServicioSaludo));
+            host.AddServiceEndpoint(typeof (IServicioSaludo), new NetTcpBinding(), "net.tcp://localhost:6985/saludo");
+
+            host.AddServiceEndpoint(typeof (IServicioSaludo), new NetTcpRelayBinding(),
+                ServiceBusEnvironment.CreateServiceUri("sb", "demo48707", "saludo")).Behaviors.Add(new TransportClientEndpointBehavior()
+                {
+                    TokenProvider = TokenProvider.CreateSharedSecretTokenProvider("owner",
+                    "gNZ5qIyZ7cZilcfAhnDrLmGYXJ0j9Xm66FXkV8ZM/KQ=")
+                });
+            host.Open();
+            Console.WriteLine("Terminar....");
+            Console.ReadLine();
+
+            host.Close();
+
+        }
+    }
+}
---
+++ ServicioServiceBus/ServicioSaludo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicioServiceBus
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioSaludo" in both code and config file together.
+    public class ServicioSaludo : IServicioSaludo
+    {
+        private Dictionary<String,String> saludos =new Dictionary<string, string>()
+        {
+            {"es","Buenos dias"},
+            {"en","Good Morning"},
+            {"fr","Bon Jour"},
+            {"de","Guten Morgen"}
+        };
+        
+      
+        public string GetSaludo(string idioma)
+        {
+            if (saludos.ContainsKey(idioma))
+                return saludos[idioma];
+
+            return saludos["en"];
+        }
+    }
+}
